190b8d04769a8ef4b41224c5402bfaf59785d92c
Keshav <keshav.oza@ellicium.com>
2024-06-18 10:40:30 +0530

    Added connect and disconnect event listners

--- server/server.js
+++ server/server.js
@@ -1,17 +1,33 @@
-
 import path from "path";
 import { fileURLToPath } from "url";
-import express from 'express'
-const port=process.env.PORT || 3000
+import express from "express";
+import { Server as socketIO } from "socket.io";
+import http from "http";
+
+const port = process.env.PORT || 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const publicPath = path.join(__dirname, "/../public"); 
+const app = express();
+let server = http.createServer(app);
 
-const publicPath=path.join(__dirname, "/../public") //Comment
+let io = new socketIO(server);
+app.use(express.static(publicPath)); 
 
-const app=express()
+io.on('connection',(socket)=>{
+    console.log("A new user just connected");
 
-app.use(express.static(publicPath))
-app.listen(port,()=>{
-    console.log(`Sever is running pn port ${port}`);
+    socket.on('disconnect',()=>{
+        console.log('User was disconnected');
+     })
+    
 })
+
+
+
+server.listen(port, () => {
+  console.log(`Sever is running on port ${port}`);
+});
+
+
